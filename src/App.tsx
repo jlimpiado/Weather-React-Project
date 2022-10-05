1693a1275d3f39957c163c50454f03c9cc490eea
d3vlimpiado <jianlimpiado@gmail.com>
2022-10-05 10:09:35 +0800

    fix bug day/s and update style

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import React from "react"
-import { ToggleButton, Card, CurrentCard, SearchBox } from "./components"
-import type { CityWeather } from "./components/SearchBox"
+import React from "react";
+import { ToggleButton, Card, CurrentCard, SearchBox } from "./components";
+import type { CityWeather } from "./components/SearchBox";
 
 function App(): JSX.Element {
-  const [selectedCity, setSelectedCity] = React.useState<CityWeather | null>(null)
-  const [selectedDay, setSelectedDay] = React.useState<number>(0)
-
-  React.useEffect(() => {
-    console.log(selectedCity)
-  }, [selectedCity])
+  const [selectedCity, setSelectedCity] = React.useState<CityWeather | null>(null);
+  const [selectedDay, setSelectedDay] = React.useState<number>(0);
 
   return (
-    <div className="text-slate-800 dark:text-slate-200 grid grid-rows-6 min-h-screen">
-      <div className="row-span-1 flex items-center justify-center">
+    <div className="text-slate-800 dark:text-slate-200 grid grid-rows-[150px_minmax(200px,_1fr)_150px] min-h-screen">
+      <div className="flex items-center justify-center">
         <SearchBox setSelectedCity={setSelectedCity} />
       </div>
-      <div className="row-span-4">
-        {selectedCity?.name && selectedCity?.country && selectedCity?.weather && selectedCity.weather[selectedDay]?.dateForecast && selectedCity.weather[selectedDay]?.description && selectedCity.weather[selectedDay]?.iconId && selectedCity.weather[selectedDay]?.temp && selectedCity.weather[selectedDay]?.tempMax && selectedCity.weather[selectedDay]?.tempMin && selectedCity.weather[selectedDay]?.humidity ? (
+      <div className="">
+        {selectedCity?.name &&
+        selectedCity?.country &&
+        selectedCity?.weather &&
+        selectedCity.weather[selectedDay]?.dateForecast &&
+        selectedCity.weather[selectedDay]?.description &&
+        selectedCity.weather[selectedDay]?.iconId &&
+        selectedCity.weather[selectedDay]?.temp &&
+        selectedCity.weather[selectedDay]?.tempMax &&
+        selectedCity.weather[selectedDay]?.tempMin &&
+        selectedCity.weather[selectedDay]?.humidity ? (
           <>
             <CurrentCard
               cityName={{ city: selectedCity?.name, country: selectedCity?.country }}
@@ -30,23 +35,29 @@ function App(): JSX.Element {
                 humidity: selectedCity.weather[selectedDay]?.humidity,
               }}
             />
-            <div className="flex overflow-auto w-screen gap-4 p-2 mt-4 sm:justify-center">
-            {selectedCity.weather.map((city, index) => (
-              <Card code={city.iconId || ""} day={index} setSelectedDay={setSelectedDay} temperature={Math.floor(city.temp || 0)} index={index} />
-            ))}
+            <div className="flex overflow-auto gap-4 p-2 mt-4 sm:justify-center">
+              {selectedCity.weather.map((weather, index) => (
+                <Card
+                  code={weather.iconId || ""}
+                  day={weather.dateForecast?.getUTCDay() || index}
+                  setSelectedDay={setSelectedDay}
+                  temperature={Math.floor(weather.temp || 0)}
+                  index={index}
+                />
+              ))}
             </div>
           </>
         ) : (
           <CurrentCard />
         )}
       </div>
-      <footer className="row-span-1 flex flex-col justify-end items-center">
+      <footer className="flex flex-col justify-end items-center">
         <ToggleButton />
         <span>COPYRIGHT &copy; 2022 JOSHUA B. LIMPIADO</span>
         <span>All rights reserved</span>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
